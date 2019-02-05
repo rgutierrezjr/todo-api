d446e5eee653d86c07f5a293f0d06540c9f26b2e
ruben <ruben@bigboston.io>
2019-02-04 22:01:27 -0600

    express, todo endpoint impl.

---
+++ server/server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    console.log(req.body);
+
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.post('/users', (req, res) => {
+    console.log(req.body);
+});
+
+app.listen(3000, () => {
+    console.log('server started on port 3000');
+});
